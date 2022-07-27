d8abf3a74da3459c3ab04e200dd699fd154812f2
unknown <bittufardeen@gmail.com>
2022-07-27 09:24:59 +0530

    ArrayProblems

---
+++ Demo4.java
@@ -0,0 +1,13 @@
+package bridgelabz;
+public class Demo4
+{
+    public static void main(String[] args)
+    {
+        String[ ] names = {"Mohd", "Fardeen", "Khan", "Aman", "Aakash"};
+
+        for (int i = 0; i < names.length; i++)
+        {
+            System.out.println( names[i] );
+        }
+    }
+}
